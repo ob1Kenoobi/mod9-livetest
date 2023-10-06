0355686d0e9daefa53f155c9f30f638d60f2435c
Hasan Banna <hansolo132134@gmail.com>
2023-10-06 23:30:55 +0600

    added the buttons and README file.

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # mod9_livetest
 
-A new Flutter project.
 
-## Getting Started
+# Size Selector App
 
-This project is a starting point for a Flutter application.
+This Flutter application demonstrates a simple UI where buttons change color upon selection. It also displays a Snackbar with the selected size text upon button press.
 
-A few resources to get you started if this is your first Flutter project:
+## Features
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- Buttons for sizes: S, M, L, XL, XXL, XXXL
+- Button color changes on selection
+- Snackbar displays the selected size
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Screenshots
+
+![Screenshot](screenshot.png)
+
+
+## Usage
+
+- Tap on the buttons to change the color.
+- A Snackbar will appear at the bottom displaying the selected size.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,108 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyScreen(),
+    );
+  }
+}
+
+class MyScreen extends StatefulWidget {
+  @override
+  _MyScreenState createState() => _MyScreenState();
+}
+
+class _MyScreenState extends State<MyScreen> {
+  String selectedSize = '';
+
+  void _showSnackBar(String size) {
+    setState(() {
+      selectedSize = size;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected Size: $size'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+
+  ElevatedButton _buildButton(String size) {
+    return ElevatedButton(
+      onPressed: () => _showSnackBar(size),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            return selectedSize == size ? Colors.orange : Colors.grey;
+          },
+        ),
+        minimumSize: MaterialStateProperty.all<Size>(
+          Size(100.0, 50.0), // Adjust width and height for the desired size
+        ),
+      ),
+      child: Text(
+        size,
+        style: TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Size Selector'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: _buildButton('S'),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: _buildButton('M'),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: _buildButton('L'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: _buildButton('XL'),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: _buildButton('XXL'),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: _buildButton('XXXL'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
